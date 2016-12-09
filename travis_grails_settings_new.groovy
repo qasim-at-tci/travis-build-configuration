9cb64627d2207beb84057dedd6179f140d68e70f
Dave Martin <djtfmartin@users.noreply.github.com>
2016-12-09 21:11:55 +0000

    updated url

--- travis_grails_settings_new.groovy
+++ travis_grails_settings_new.groovy
@@ -1,7 +1,7 @@
-grails.project.repos.'uk-nbn-snapshots'.url="http://repository.gbif.org/content/repositories/snapshots/"
+grails.project.repos.'uk-nbn-snapshots'.url="http://repository.gbif.org/content/repositories/uk-nbn-snapshots"
 grails.project.repos.'uk-nbn-snapshots'.username=System.getenv("TRAVIS_DEPLOY_USERNAME")
 grails.project.repos.'uk-nbn-snapshots'.password=System.getenv("TRAVIS_DEPLOY_PASSWORD")
 
-grails.project.repos.'uk-nbn-releases'.url="http://repository.gbif.org/content/repositories/releases/"
+grails.project.repos.'uk-nbn-releases'.url="http://repository.gbif.org/content/repositories/uk-nbn-releases"
 grails.project.repos.'uk-nbn-releases'.username=System.getenv("TRAVIS_DEPLOY_USERNAME")
 grails.project.repos.'uk-nbn-releases'.password=System.getenv("TRAVIS_DEPLOY_PASSWORD")
