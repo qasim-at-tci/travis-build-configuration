65cadf7dc534b0737e29f5ef772b9fdb6b5152f7
Dave Martin <djtfmartin@users.noreply.github.com>
2016-12-12 11:16:44 +0000

    Update travis_grails_settings_old.groovy

--- travis_grails_settings_old.groovy
+++ travis_grails_settings_old.groovy
@@ -1,9 +1,9 @@
 grails.project.dependency.distribution = {
-  remoteRepository(id:"uk-nbn-snapshots", url:"http://repository.gbif.org/content/repositories/snapshots") {
+  remoteRepository(id:"uk-nbn-snapshots", url:"http://repository.gbif.org/content/repositories/uk-nbn-snapshots") {
     authentication username: System.getenv("TRAVIS_DEPLOY_USERNAME"), password: System.getenv("TRAVIS_DEPLOY_PASSWORD")
   }
 
-  remoteRepository(id:"uk-nbn-release",  url:"http://repository.gbif.org/content/repositories/releases") {
+  remoteRepository(id:"uk-nbn-release",  url:"http://repository.gbif.org/content/repositories/uk-nbn-releases") {
     authentication username: System.getenv("TRAVIS_DEPLOY_USERNAME"), password: System.getenv("TRAVIS_DEPLOY_PASSWORD")
   }
 }
